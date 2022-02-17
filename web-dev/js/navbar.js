cf9ad016e84384a7e00fbba853373d233ba8fc5a
Chaidul <technicalgyanassam@gmail.com>
2022-02-17 15:28:05 +0530

    added responsive navbar

---
+++ web-dev/js/navbar.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	let toggle = document.getElementById("nav-toggle");
+	let navUl = document.querySelector(".nav-ul")
+	toggle.addEventListener("click",function(){
+		navUl.classList.toggle("hide-ul")
+	
+	
+	})
+
+
+
+
+
+}
